c007e9fce553fdb47f419caf9d571385856c6704
root <root@vm1.thoughtworks.com>
2012-05-10 21:59:38 +0530

    Addede test cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+package "httpd" do
+                   action :install
+                end
+service "httpd" do
+
+                   action [:enable, :start]
+                end
+   
